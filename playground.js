e988d96b00db8230867b62ae8b7a4db4a75e99d6
Sean McCullough <banksean@gmail.com>
2024-06-05 19:29:56 -0700

    some ai-genrated code, some not

---
+++ playground.js
@@ -0,0 +1,23 @@
+// This is a playground for testing generative code suggestion logging.
+
+// This is hand-written.
+
+function foo() {
+  return 'bar';
+}
+
+// This is the generative code suggestion.
+
+function fib(n) {
+  if (n <= 1) {
+    return n;
+    } else {
+    return fib(n - 1) + fib(n - 2);
+  }
+}
+
+// And this is more hand-written code.
+
+function bar() {
+  return 'foo';
+}
